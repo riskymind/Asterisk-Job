fe0ba21c54ddc6c38a468f2573e8f1af8acd6c9e
kelechi <oparaiskelechi@gmail.com>
2024-08-14 06:40:07 +0100

    added the homecards for jobs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import HomeCard from './components/HomeCard'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <HomeCard />
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = ({
     subtitle = "Find the right job that fits your skill set"
 }) => {
   return (
-    <section className='bg-indigo-700 py-20'>
+    <section className='bg-indigo-700 py-20 mb-4'>
       <div className='max-w-7xl mx-auto px-4 flex flex-col items-center sm:px-6 lg:px-8'>
         <div className='text-center'>
             <h1 className='text-4xl text-white font-extrabold sm:text-x5l md:text-6xl'>{title}</h1>
