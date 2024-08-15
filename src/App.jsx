634b710fb7469d1198ac9c7e38b9c162fb317dc1
kelechi <oparaiskelechi@gmail.com>
2024-08-15 14:43:06 +0100

    added job to the server

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,23 @@ import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoadder } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 
 const App = () => {
+
+  // Add New Job
+  const addJob = async (newJob) => {
+      const res = await fetch("/api/jobs", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(newJob)
+      });
+      return
+  }
 
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -21,6 +35,8 @@ const App = () => {
         <Route index element={<HomePage/>} />
         <Route path='/jobs' element={<JobsPage/>} />
         <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoadder} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+        <Route path='/edit-job/:id' element={<EditJobPage/>} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     )
